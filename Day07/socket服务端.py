bb6886e1f84c0d997e98294c39d5c4e00ab1d332
Administrator <1155090@qq.com>
2017-05-09 18:40:35 +0800

    做了一个简单socket连接

--- Day07/socket服务端.py
+++ Day07/socket服务端.py
@@ -8,14 +8,15 @@ server = socket.socket()
 server.bind(('localhost',9999))#里面要用一个元组的形式来写
 #监听
 server.listen()
-print('等待请求')
 #等待请求，把来的请求做成一个实例和标记地址
 while True:#这里是为了可以接收下一个socket的请求
+    print('等待请求')
     conn,addr = server.accept()# conn就是客户端连过来而在服务器端为其生成的一个连接实例
     print('接收到的地址是',addr)
     #把接收到的数据保存
     #如果想不停的接收和返回的话那么就不能断开socket，这里就来一个循环
     while True:
+        print('开始接收数据')
         data = conn.recv(1024) #记得这里是实例在负责发送和接收也就是conn
 
         print('接收到数据%s',data)
@@ -28,8 +29,15 @@ while True:#这里是为了可以接收下一个socket的请求
         #然后返回处理结果
         print(data)
         data = data.encode('utf-8')
+        data_size = str(len(data)).encode()
+        #先告诉对方data 的大小
+        conn.send(data_size)
+        if len(data) == 0:
+            conn.send('您发送的数据有问题'.encode())
+            continue
+        #再发送data本身
         conn.send(data)
-        print('返回',data)
+        print('返回',data_size)
     #那么如果发完了就关掉的话是不是不能接收下一个请求了啊？如果想的话我们就需要再建立一个实例也就是conn
 #发完了就关闭这次socket
 server.close()
--- Day07/客户端.py
+++ Day07/客户端.py
@@ -14,7 +14,24 @@ count = 1
 for i in range(30):
     data = input('输入你要的：')
     client.send(data.encode('utf-8'))
-    #接着就可以收了
-    data = client.recv(1024)
-    print('recv:',data.decode())
+    #接着就可以收了,但是这时候是不是应该想如果一次接收不完是不是应该继续接收？是的
+    print('开始接收数据的大小')
+    #第一次是接收到data的大小
+    data_size = int(client.recv(1024).decode())
+    print('数据文件大小为',data_size)
+
+    #然后就开始判断什么时候才可以把data都收完
+    recv_data = ''
+    data_recv_all_size = -1
+    while data_recv_all_size < data_size :
+        #开始接收数据
+        print('开始接收数据')
+        recv_data = client.recv(1024)
+        #判断数据大小
+        print('判断数据大小')
+        data_recv_size = len(recv_data)
+        data_recv_all_size += data_recv_size
+
+        print('接收到的数据为：',recv_data.decode())
+    print('已经接收到的文件大小为:', data_recv_all_size)
 print('结束会话')
